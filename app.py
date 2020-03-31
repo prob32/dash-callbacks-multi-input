7f7944598eddf27113ce32d2597d56f23927e845
prob32 <patrick.s.robinson@maine.edu>
2020-03-31 15:59:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,19 +9,13 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
+
 #app inputs
-app.layout = html.Div(children=[
-        html.H1(children='Shipping Estimates',
-        style = {'color': colors['text']}),
-        html.Div(children='''Created by: Patrick Robinson'''),
-    
-    ## enterbox one one 1
-    html.Label('Enter your income')
-    dcc.Input(id='my-id', value='initial value', type='number'),
+app.layout = html.Div([
+    dcc.Input(id='my-id', value='initial value', type='text'),
     html.Div(id='my-div')
 ])
-
-
+#Cakk backs
 @app.callback(
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
