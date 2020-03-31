9fc24b8ed80adde0553278c92c1ad7657c18ba33
prob32 <patrick.s.robinson@maine.edu>
2020-03-31 15:55:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,32 +4,30 @@ import dash_core_components as dcc
 ##
 
 tabtitle = 'COVID-19 Income tax calculator'
-###
+###Configure app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
-
-app.layout = html.Div([
-    dcc.Dropdown(
-        id='demo-dropdown',
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': 'Montreal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        value='NYC'
-    ),
-    html.Div(id='dd-output-container')
+#app inputs
+app.layout = html.Div(children=[
+        html.H1(children='Shipping Estimates',
+        style = {'color': colors['text']}),
+        html.Div(children='''Created by: Patrick Robinson'''),
+    
+    ## enterbox one one 1
+    html.Label('Enter your income')
+    dcc.Input(id='my-id', value='initial value', type='number'),
+    html.Div(id='my-div')
 ])
 
 
 @app.callback(
-    dash.dependencies.Output('dd-output-container', 'children'),
-    [dash.dependencies.Input('demo-dropdown', 'value')])
-def update_output(value):
-    return 'You have selected "{}"'.format(value)
-
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server()
