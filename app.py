6b8afd2da5e46c18b5820ed6083df0f3ef71bcd1
prob32 <patrick.s.robinson@maine.edu>
2020-03-31 15:29:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 
+server = app.server
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
